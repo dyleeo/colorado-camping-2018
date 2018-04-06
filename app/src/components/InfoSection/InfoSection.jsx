4667234567ce629b19513117c23a09cb846cc326
dyleeo <danny@dyleeo.com>
2018-04-06 01:56:10 -0400

    added maps

--- app/src/components/InfoSection/InfoSection.jsx
+++ app/src/components/InfoSection/InfoSection.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
+import Map from '../Map/Map';
+
 import './InfoSection.scss';
 
 class InfoSection extends Component {
+
 
   render() {
     return(
@@ -13,20 +16,23 @@ class InfoSection extends Component {
         <section className="info-section">
 
           <h2>It's Colorado or bust!</h2>
-          It started as a nonchalant challenge from Tito and Sewon during a fun-filled<br/>family gathering, and so planted was the seed of what this "camping" trip has evolved into.
+          <p>It started as a nonchalant challenge from Tito and Sewon during a fun-filled family gathering, and so planted was the seed of what this "camping" trip has evolved into.</p>
 
 
           <div className="panels-container">
+
             <article className="when">
               <header>WHEN</header>
-              <div className="content-container">
-                August 4th, 2018
+              <div className="b-content-container">
+                <h1>August 4th, 2018</h1>
               </div>
             </article>
 
             <article className="where">
               <header>WHERE</header>
-              <div className="content-container">aaa</div>
+              <div className="b-content-container map-box">
+                <Map  />
+              </div>
             </article>
 
           </div>
@@ -41,3 +47,5 @@ class InfoSection extends Component {
 }
 
 export default InfoSection;
+
+//const API_KEY = "AIzaSyAKVbiPguvPjEeOLOmipeErhsnWQSgi22s";
---
+++ app/src/components/Map/Map.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import dotenv from 'dotenv'
+dotenv.config({path: './'})
+
+const AnyReactComponent = ({ text }) => <div >{text}</div>; //this can be styled or used with another comp
+
+class Map extends Component {
+
+  render() {
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: [process.env.GOOGLE_MAPS_API_KEY] }}
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      >
+        <AnyReactComponent
+          lat={39.6528607}
+          lng={-106.1162855}
+          text={'Silverthorne, Colorado'}
+        />
+      </GoogleMapReact>
+    );
+  }
+}
+
+Map.defaultProps = {
+  center: {
+    lat: 39.6528607,
+    lng: -106.1162855
+  },
+  zoom: 7
+};
+
+export default Map;
