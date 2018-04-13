b843fdf0cd15ddc75f1887ec4f420a2f3c528cb1
dyleeo <danny@dyleeo.com>
2018-04-12 22:09:37 -0400

    added mor info

--- app/src/components/InfoSection/InfoSection.jsx
+++ app/src/components/InfoSection/InfoSection.jsx
@@ -15,7 +15,7 @@ class InfoSection extends Component {
     return(
       <div id="info">
 
-        <h1>Information</h1>
+        <h1 className="header-banner">Information</h1>
 
         <section className="info-section">
 
@@ -48,6 +48,7 @@ class InfoSection extends Component {
 
         <section className="venue-container">
 
+
             <div className="b-content-container tent-container">
               <div className="tent-header">
                 <div className="airbnb-logo"><img src={airbnb}/></div>
@@ -57,8 +58,92 @@ class InfoSection extends Component {
               <div className="visuals">
                 <div className="house-pic shadow"><img src={tent}/></div>
               </div>
-            </div>
 
+              <div className="tent-amenities">
+                <h1>Amenities</h1>
+                <div className="row">
+                  <div className="info-col col-xs-12 col-md-4 col-lg-4 ">
+                    <h3>Basic</h3>
+                    <div className="listing">
+                      -Dryer<br />
+                      <div className="sub">In the building, free or for a fee</div><br />
+
+                      -Essentials<br />
+                      <div className="sub">Towels, bed sheets, soap, and toilet paper</div><br />
+
+                      -Indoor fireplace<br /><br />
+
+                      -Heating<br />
+                      <div className="sub">Central heating or a heater in the listing</div><br />
+
+                      -Hot water<br /><br />
+
+                      -Iron<br /><br />
+
+                      -Laptop friendly workspace<br />
+                      <div className="sub">A table or desk with space for a laptop and a chair that’s comfortable to work in</div><br />
+
+                      -TV<br /><br />
+
+                      -Washer<br />
+                      <div className="sub">In the building, free or for a fee</div><br />
+
+                      -Wifi<br />
+                      <div className="sub">Continuous access in the listing</div><br />
+                    </div>
+                  </div>
+
+                  <div className="info-col col-xs-12 col-md-4 col-lg-4">
+                    <h3>Family features</h3>
+                    <div className="listing">
+                      -Baby monitor<br />
+                      -Bathtub<br />
+                      -Children’s books and toys<br />
+                      -Crib<br />
+                      -High chair<br />
+                      -Pack ’n Play/travel crib<br />
+                      -Stair gates<br />
+
+                      <h3>Facilities</h3>
+                      -Free parking on premises<br />
+                      -Gym<br />
+                      -Free, in the building or nearby<br />
+                      -Hot tub<br /><br />
+
+
+                      <h3>Dining</h3>
+                      -Kitchen<br />
+                      <div className="sub">Space where guests can cook their own meals</div>
+                    </div>
+                  </div>
+
+                  <div className="info-col col-xs-12 col-md-4 col-lg-4">
+
+                    <h3>Guest access</h3>
+                    <div className="listing">
+                      -Keypad
+                      <div className="sub">A keypad must be available 24 hours a day to provide entry to your listing for your guest</div><br />
+
+                      -Private entrance
+                      <div className="sub">Separate street or building entrance</div>
+
+                      <h3>Bed and bath</h3>
+                      -Hair dryer<br />
+                      -Hangers<br />
+                      -Shampoo<br />
+
+                      <h3>Safety features</h3>
+                      -Fire extinguisher<br />
+                      -Carbon monoxide detector<br />
+                      -Smoke detector<br />
+                      -First aid kit<br />
+                    </div>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
         </section>
 
       </div>
